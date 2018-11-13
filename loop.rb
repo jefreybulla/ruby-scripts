0dc2ea27d4b323adc5dfa828e01ccb2cf697c0d5
Jefrey Bulla <jefrey.bulla@gmail.com>
2018-11-12 19:44:22 -0500

    nested-arrays

--- loop.rb
+++ loop.rb
@@ -1,10 +1,24 @@
 p 'init'
 
-x = [1,2,3,4,5]
+arr = [1,2,3,4,5]
 counter = 0
-x.each do
-  x[counter] = 100
-  counter += 1
+
+arr.each do |i|
+  puts i
 end
 
-p x
+arr.each do
+  arr[counter] = 100
+  counter += 1
+end
+p arr
+
+
+computers = {make:"apple",model:"macbook"}
+computers.each do |key, value|
+  puts "#{key} => #{value}"
+end
+
+10.times do
+  puts 1
+end
---
+++ nested-arrays.rb
@@ -0,0 +1,8 @@
+arr = [[1,2,3],[4,5,6]]
+puts arr[0][1]
+
+phones = {
+  iphonex:{year:2017, touch:"yes"},
+  blackberry:{year:2000, touch:"no"}
+}
+puts phones[:blackberry][:touch]
