5356ded77867b405a711915a1caec84ab2d3fc8a
Jefrey Bulla <jefrey.bulla@gmail.com>
2018-11-14 17:56:57 -0500

    letter-frequency

---
+++ letter-frequency.rb
@@ -0,0 +1,14 @@
+text = "Check how often characters are repeated on this string"
+text.downcase!
+freqs = {}
+freqs.default = 0
+
+text.each_char do |char|
+  freqs[char] += 1
+end
+
+puts freqs
+
+("a".."z").each do |x|
+  puts x + " => #{freqs[x]}"
+end
