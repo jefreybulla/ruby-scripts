adf26b3a7ee36da03b5adf91f7c95e47e9c374d9
Jefrey Bulla <jefrey.bulla@gmail.com>
2018-11-26 16:08:44 -0500

    methods +

--- method.rb
+++ method.rb
@@ -4,3 +4,31 @@ end
 x= 4
 y= 5
 product(x,y)
+
+def manyArguments(*p) #any number of arguments
+  puts p
+  puts p[1]
+end
+
+manyArguments(2,100,"hola")
+
+def temp
+  return "my message"
+end
+
+t = temp
+puts t
+
+def squares (a,b)
+  return a*a, b*b  #return multiple values
+end
+
+puts squares(2,3)
+
+def sq(x)
+  x*x
+end
+
+sq(4).times{
+  puts "Hi"
+}
