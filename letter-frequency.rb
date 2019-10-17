4cf4d27634e31a18be4ce9b10940f0e8f6e0ab07
Jefrey Bulla <jefrey.bulla@gmail.com>
2019-10-17 18:00:05 -0400

    add comments

--- letter-frequency.rb
+++ letter-frequency.rb
@@ -1,14 +1,17 @@
 text = "Check how often characters are repeated on this string"
 text.downcase!
-freqs = {}
-freqs.default = 0
+freqs = {}          #create empty hash
+freqs.default = 0   #set the default of all values to 0
 puts freqs
+puts "--------"
 
 text.each_char do |char|
   freqs[char] += 1
 end
 
 puts freqs
+
+puts "--------"
 
 ("a".."z").each do |x|
   puts x + " => #{freqs[x]}"
