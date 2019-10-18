df3e96a9526dc15d9648e00c5b01b07329082d20
Jefrey Bulla <jefrey.bulla@gmail.com>
2019-10-18 08:05:20 -0400

    add more method cases

--- method.rb
+++ method.rb
@@ -1,18 +1,32 @@
+###Ruby Methods###
+
+#a, b are the parameters .x,y are the arguments.
 def product (a,b=2)
   puts a*b
 end
 x= 4
 y= 5
 product(x,y)
-#product(x)  #use first parameter and take second paremeter with the default value
+product(x)  #send one argument and use the default value for the second one.
 
-def manyArguments(*p) #any number of arguments
+puts "-------------"
+
+#Method that aceppts any number of parameters. p is an array of any size.
+def acceptMany(*p)
   puts p
   puts p[1]
 end
 
-manyArguments(2,100,"hola")
+#Method with two mandatory parameters and one optional parameter
+def optionalParameters(a,b,*c)
+  #some code
+end
 
+acceptMany(2,100,"hola")
+
+puts "-------------"
+
+#Method that returns a value. If return statement is not present, a method always return the last line of code.
 def temp
   return "my message"
 end
@@ -20,32 +34,48 @@ end
 t = temp
 puts t
 
+#Method that returns multile values
 def squares (a,b)
   return a*a, b*b  #return multiple values
 end
-
 puts squares(2,3)
 
 def sq(x)
   x*x
 end
 
-sq(2).times{
+sq(2).times do
   puts "Hi"
-}
+end
 
+#Ruby has four types of variable scope: local, global, instance and class.
+#local variable are available only in the context where they were called (a method or loop)
+#Use of global variables is strongly discouraged
+#dollar sign makes a variable a global variable.
 def change
-  $global = 99  #dollar sign makes a variable a global variable.
+  $global = 99
 end
 change
 puts $global
 
 #Recursiveness
 def factorial(n)
-  if n <= 1   # this is the base case. Without it, loop is infinate. 
+  if n <= 1   # this is the base case. Without it, loop is infinite.
     1
   else
     n*factorial(n-1)
   end
 end
-puts 'factorial is: ' + factorial(6).to_s
+puts 'factorial is: ' + factorial(4).to_s
+
+=begin
+def greet(name="")
+  if name == ""
+    puts "Greetings"
+  else
+    puts "Welcome, #{name}"
+  end
+end
+
+greet(gets.chomp)     #request user input via the terminal
+=end
