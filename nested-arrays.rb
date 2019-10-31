52f71397193940a75b7b57c6f40c3cd56879c9a4
Jefrey Bulla <jefrey.bulla@gmail.com>
2019-10-31 14:22:34 -0400

    add comments and fix copy

--- nested-arrays.rb
+++ nested-arrays.rb
@@ -1,5 +1,8 @@
+#nested array: an array of arrays
 arr = [[1,2,3],[4,5,6]]
 puts arr[0][1]
+
+#nested hash: a has of hashes
 
 phones = {
   iphonex:{year:2017, touch:"yes"},
--- user-input.rb
+++ user-input.rb
@@ -2,6 +2,6 @@ x = ""
 while x != "STOP" do
   puts "Hi, how are you feeling?"
   ans = gets.chomp
-  puts "Want me to ask again"
+  puts "Wanted to ask again.."
   x = gets.chomp
 end
