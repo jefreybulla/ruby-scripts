ba2952ec72831db3a14437eb8f3a3b0cc175ec92
Jefrey Bulla <jefrey.bulla@gmail.com>
2019-12-10 20:25:09 -0800

    add source to challenge

--- challenges/minimum-swaps.rb
+++ challenges/minimum-swaps.rb
@@ -1,6 +1,8 @@
 #You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements.
 #You need to find the minimum number of swaps required to sort the array in ascending order.
 
+#source: https://www.hackerrank.com/challenges/minimum-swaps-2/problem
+
 def swap(arr)
   for i in 0..arr.length-1
     if arr[i] == i+1
