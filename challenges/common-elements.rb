433041768a1b654c50313e6c83041f66ecfcd7f2
Jefrey Bulla <jefrey.bulla@gmail.com>
2019-12-11 11:16:08 -0800

    add runing times

--- challenges/common-elements.rb
+++ challenges/common-elements.rb
@@ -2,25 +2,22 @@
 
 # O(A*B)
 def findCommonElements(a,b)
+  start = Time.now
   result = []
-  runCounter = 0
   a.each do |itemA|
     b.each do |itemB|
-      runCounter += 1
       if itemA == itemB
         result.push(itemA)
       end
     end
   end
-  puts "This loop ran #{runCounter} times"
-  return result
+  executionTime = Time.now - start
+  return result, executionTime
+
 end
 
 def checkOnB(itemA,b)
-  runCounter = 0
   b.each do |itemB|
-    runCounter += 1
-    puts "runCounter: #{runCounter}"
     if itemA == itemB
       return itemA
     end
@@ -28,25 +25,29 @@ def checkOnB(itemA,b)
   return false
 end
 
-# faster than O(A*B) since it stops after a match is found
+#stoping after a match is found
 def findCommonElements2(a,b)
+  start = Time.now
   result = []
   a.each do |itemA|
     if checkOnB(itemA,b)
       result.push(itemA)
     end
   end
-  return result
+  executionTime = Time.now - start
+  return result, executionTime
 end
 
 #Method that using uses ruby method
 def findCommonElements3(a,b)
-  return a & b
+  start = Time.now
+  executionTime = Time.now - start
+  return a & b, executionTime
 end
 
 a = [1,5,12,3,-15,52]
 b = [3,1,6,5,57,13,17]
 
-#puts findCommonElements(a,b)  #42 cycles
-#puts findCommonElements2(a,b)  #28 cycles
-puts findCommonElements3(a,b)
+#puts findCommonElements(a,b)  #42 cycles 8us
+#puts findCommonElements2(a,b)  #28 cycles  7us
+puts findCommonElements3(a,b)   # 1us - Winning time!
