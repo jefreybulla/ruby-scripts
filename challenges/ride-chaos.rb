d37c5b5ae3bfac2fca052408f58e7ff45fb29957
Jefrey Bulla <jefrey.bulla@gmail.com>
2019-12-10 18:07:43 -0800

    add comments

--- challenges/ride-chaos.rb
+++ challenges/ride-chaos.rb
@@ -1,4 +1,8 @@
-#success array has elements with one increment [1,2,3] or [1,2,3,4,5,6] or [1,2,3,4,5,6,7,8,9,10]
+#Everyone's in line for the Wonderland rollercoaster ride. There are a number of people queued up, and each person wears a sticker indicating their initial position in the queue. Initial positions increment by  from  at the front of the line to  at the back.
+#Any person in the queue can bribe the person directly in front of them to swap positions. If two people swap positions, they still wear the same sticker denoting their original places in line. One person can bribe at most two others. For example, if n=8 and Person 5 bribes Person 4, the queue will look like this: 1,2,3,5,4,6,7,8.
+#Calculate the minimum number of bribes that took place to get the queue into its current state.
+
+#From: https://www.hackerrank.com/challenges/new-year-chaos/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
 
 def calculateBribes(q)
   bribes = 0
@@ -7,13 +11,13 @@ def calculateBribes(q)
   checkNumber.default = 0
   for i in 0..(q.length-2)
     if q[i] < q[i+1]
-      puts "in order"
+      #puts "in order"
     else
+      #puts "not in oder"
       checkNumber[q[i]] += 1
       if checkNumber[q[i]] > 2
         return "Too caothic"
       end
-      puts "needs calculation"
       a = q[i] #5
       b = q[i+1]  #3
       q[i] = b
@@ -29,9 +33,6 @@ end
 #finalQ = [2,1,4,3]  #2 bribes
 #finalQ = [1,2,5,3,4]  #2 bribes
 finalQ = [2,1,5,3,4]  #3 bribes
-#finalQ = [2,1,6,3,4,5]  #3 bribes
-#1 2 3 5 4
-
-
+#finalQ = [2,1,6,3,4,5]  #Too chaotic
 
 puts calculateBribes(finalQ)
