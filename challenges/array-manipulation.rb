1500c47c48e38fb8dd0b5f5fdcacbb651085f366
Jefrey Bulla <jefrey.bulla@gmail.com>
2019-12-10 22:41:24 -0800

    add comments

--- challenges/array-manipulation.rb
+++ challenges/array-manipulation.rb
@@ -1,4 +1,6 @@
-
+#Starting with a 1-indexed array of zeros and a list of operations, for each operation add a value to each of the array element between two given indices, inclusive.
+#Once all operations have been performed, return the maximum value in your array.
+#Source: https://www.hackerrank.com/challenges/crush/problem
 
 def arrayManipulation(n,queries)
   arr = Array.new(n,0)
